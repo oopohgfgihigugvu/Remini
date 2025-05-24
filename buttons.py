dc5e64b84c586394cfba468c4cbc69725004cfab
UTTAM RATHORE <uk977061@gmail.com>
2025-05-24 13:18:03 +0530

    V3.0

--- buttons.py
+++ buttons.py
@@ -1,4 +1,3 @@
-# button.py
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 def get_adjustment_keyboard(final_image_path=None):
@@ -9,8 +8,6 @@ def get_adjustment_keyboard(final_image_path=None):
          InlineKeyboardButton("⇃ᴅᴏᴡɴ", callback_data="move_down")],
         [InlineKeyboardButton("⛶ ✙", callback_data="increase_size"),
          InlineKeyboardButton("⛶ –", callback_data="decrease_size")],
-        
-        # Color selection buttons
         [InlineKeyboardButton("🔴", callback_data="color_red"),
          InlineKeyboardButton("🔵", callback_data="color_blue"),
          InlineKeyboardButton("🟢", callback_data="color_green"),
@@ -18,20 +15,13 @@ def get_adjustment_keyboard(final_image_path=None):
          InlineKeyboardButton("🟡", callback_data="color_yellow"),
          InlineKeyboardButton("🟠", callback_data="color_orange"),
          InlineKeyboardButton("🟣", callback_data="color_purple")],
-        
-        # Font selection buttons
         [InlineKeyboardButton("🄵ᴀ", callback_data="font_deadly_advance_italic"),
          InlineKeyboardButton("🄵ʙ", callback_data="font_deadly_advance"),
          InlineKeyboardButton("🄵ᴄ", callback_data="font_trick_or_treats"),
          InlineKeyboardButton("🄵ᴅ", callback_data="font_vampire_wars_italic"),
          InlineKeyboardButton("🄵ᴇ", callback_data="font_lobster")],
-        
-        # Blur buttons
         [InlineKeyboardButton("ʙʟᴜʀ +", callback_data="blur_plus"),
          InlineKeyboardButton("ʙʟᴜʀ -", callback_data="blur_minus")],
-
-        # Always show the Download button
         [InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ ʏᴏᴜʀ ʟᴏɢᴏ", callback_data="download_logo")]
     ]
-    
     return InlineKeyboardMarkup(buttons)
