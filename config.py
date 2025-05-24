ede2d6ab88feb560300b60c073b410fff919f8a8
UTTAM RATHORE <uk977061@gmail.com>
2025-05-24 13:18:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,6 @@
-"""
-Configuration module for the bot status checker.
-
-This module loads environment variables required for the bot to function,
-such as bot token, API ID and API HASH.
-It also sets up the necessary configuration settings.
-"""
-
 from os import getenv
 from dotenv import load_dotenv
-
 load_dotenv()
-
 class Config:
     API_ID = int(getenv("API_ID", "16457832"))
     API_HASH = getenv("API_HASH", "3030874d0befdb5d05597deacc3e83ab")
